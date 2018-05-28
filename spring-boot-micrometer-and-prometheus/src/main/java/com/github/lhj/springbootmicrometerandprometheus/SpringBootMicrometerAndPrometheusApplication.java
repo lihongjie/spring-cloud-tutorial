16bb3566ca0ebf7e1c59e85a4e6675acbe712945
lihongjie <lhjalyw@163.com>
2018-05-28 23:37:57 +0800

    Modified pom.xml file and add project spring-boot-micrometer-and-prometheus

---
+++ spring-boot-micrometer-and-prometheus/src/main/java/com/github/lhj/springbootmicrometerandprometheus/SpringBootMicrometerAndPrometheusApplication.java
@@ -0,0 +1,12 @@
+package com.github.lhj.springbootmicrometerandprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicrometerAndPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicrometerAndPrometheusApplication.class, args);
+	}
+}
